b8d9880248d9e66432eb71876f7dc91bca0eb260
Florieta <florieta.pencheva@gmail.com>
2022-06-24 15:43:16 +0300

    Added two problem solutions about Advanced Querying Exercise

---
+++ 06.Advanced Querying/2.AgeRestriction/BookShop/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using BookShop.Models.Enums;
+
+
+namespace BookShop
+{
+    using Data;
+    using Initializer;
+    
+
+    public class StartUp
+    {
+        public static object AgeResriction { get; private set; }
+
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            AgeRestriction ageRestriction = Enum.Parse<AgeRestriction>(command, true);
+
+            string[] bookTitles = context
+                .Books
+                .Where(b => b.AgeRestriction == ageRestriction)
+                .OrderBy(b => b.Title)
+                .Select(b => b.Title)
+                .ToArray();
+
+            foreach (string title in bookTitles)
+            {
+                sb.AppendLine(title);
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
