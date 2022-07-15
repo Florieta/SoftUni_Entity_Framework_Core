9c9b7304528d75365dd10071c8739d76a19532cf
Florieta <florieta.pencheva@gmail.com>
2022-07-15 15:14:35 +0300

    Added the second problem solution of the sample exam

--- Exam Preparation/DatabasesAdvancedExam4April2021/TeisterMask/Data/Configuration.cs
+++ Exam Preparation/DatabasesAdvancedExam4April2021/TeisterMask/Data/Configuration.cs
@@ -2,6 +2,6 @@
 {
     public static class Configuration
     {
-        public static string ConnectionString = @"Server=.;Database=TeisterMask;Trusted_Connection=True";
+        public static string ConnectionString = @"Server=.;Database=TeisterMask;User Id=sa;Password = Fofi1303!";
     }
 }
