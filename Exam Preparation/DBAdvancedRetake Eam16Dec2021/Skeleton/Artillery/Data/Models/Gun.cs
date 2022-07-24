1c92b0fbfb8bb03792baafca6b8405056ca4c7ac
Florieta <florieta.pencheva@gmail.com>
2022-07-25 00:00:36 +0300

    Added some problem solution of sample exam

---
+++ Exam Preparation/DBAdvancedRetake Eam16Dec2021/Skeleton/Artillery/Data/Models/Gun.cs
@@ -0,0 +1,43 @@
+﻿using Artillery.Data.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Artillery.Data.Models
+{
+    public class Gun
+    {
+        public Gun()
+        {
+            CountriesGuns = new HashSet<CountryGun>();
+        }
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey(nameof(Manufacturer))]
+        public int ManufacturerId { get; set; }
+
+        public Manufacturer Manufacturer { get; set; }
+        [Required]
+        [MaxLength(1350000)]
+        public int GunWeight { get; set; }
+        [Required]
+        [MaxLength(35)]
+        public double BarrelLength { get; set; }
+
+        public int? NumberBuild { get; set; }
+        [Required]
+        [MaxLength(100000)]
+        public int Range { get; set; }
+        [Required]
+        public GunType GunType { get; set; }
+        [ForeignKey(nameof(Shell))]
+        public int ShellId { get; set; }
+
+        public Shell Shell { get; set; }
+
+        public ICollection<CountryGun> CountriesGuns { get; set; }
+    }
+}
