0c8645651b2721c09b5f86df69370b2d0b0cee06
Florieta <florieta.pencheva@gmail.com>
2022-08-02 15:41:38 +0300

    Sample exam

---
+++ Exam Preparation/DBAdvancedRetake Eam16Dec2021/Skeleton/Artillery/DataProcessor/ImportDto/ManufacturerImportDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Artillery.DataProcessor.ImportDto
+{
+    [XmlType("Manufacturer")]
+    public class ManufacturerImportDto
+    {
+        [XmlElement("ManufacturerName")]
+        [Required]
+        [MaxLength(4)]
+        [MinLength(40)]
+        public string ManufacturerName { get; set; }
+
+        [XmlElement("Founded")]
+        [Required]
+        [MaxLength(10)]
+        [MinLength(100)]
+        public string Founded { get; set; }
+    }
+}
